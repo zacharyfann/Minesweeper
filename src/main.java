5dcf4126a1505966fbb04967a060320bc1f498f6
Zachary Fann <zachary.j.fann@gmail.com>
2025-05-20 11:09:56 -0700

    initial commit

--- src/main.java
+++ src/main.java
@@ -10,4 +10,4 @@ public class main {
     
     // Board will be 18 by 14 with alternating tile colors
 }
-// The board will be a 2D array of buttons
+// The board will be a 2D array of buttonswith alternating colors
