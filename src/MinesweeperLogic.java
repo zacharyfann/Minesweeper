9e6f58f2db2e526e67e0187dcf11a5ccfd2af39c
Zachary Fann <zachary.j.fann@gmail.com>
2025-05-22 11:33:08 -0700

    update

--- src/MinesweeperLogic.java
+++ src/MinesweeperLogic.java
@@ -3,6 +3,7 @@ public class MinesweeperLogic {
 	
 	
 	private Tile[][] board;	
+	private boolean[][] mines = new boolean[18][14]; 
 	/*
 	 * The constructor setups up the 2D array of Tiles for the chess board
 	 * The constructor calls 3 helper methods with add chess pieces on the board
@@ -25,8 +26,8 @@ public class MinesweeperLogic {
 		while(count < numMines) {
 			int row = (int)(Math.random() * 18)+1;
 			int col = (int)(Math.random() * 14)+1;
-			if(board[row][col].getPiece() == null) {
-				board[row][col].setPiece(new Mine(row, col, board));
+			if(mines[row][col] == false) {
+				mines[row][col] = true;
 				count++;
 			}
 		}
@@ -42,7 +43,7 @@ public class MinesweeperLogic {
 					for (int x = -1; x <=1; x++) {  //this checks the x direction one tile to the left and one tile to the right
 						for(int y = -1; y<=1; y++) { //this checks the y direction one tile up and one tile down
 							if (i+x >= 0 && i+x < board.length && j+y >= 0 && j+y < board[i].length) { //this checks if the tile is within the bounds of the board (no out of bounds error)
-								if (board[i+x][j+y].getPiece() instanceof Mine) {
+								if (mines[i+x][j+y] == true) { //this checks if the tile is a mine
 									numMines++;
 								}
 								board[i][j].setPiece(new Number( numMines, i, j, board)); //set the tile to the number of mines around it
@@ -65,10 +66,10 @@ public class MinesweeperLogic {
 		
 		for(int i = 0; i < board.length;i++) {
 			for(int j = 0; j < board[i].length;j++) {
-				if (board[i][j].getPiece() instanceof Mine && board[i][j].isFlagged() == false) {
+				if (mines[i][j] == true && board[i][j].isFlagged() == false) {
 					return false;
 				}
-				else if (board[i][j].getPiece() instanceof Number && board[i][j].isFlagged() == true) {
+				else if (board[i][j] == false && board[i][j].isFlagged() == true) {
 					return false;
 				}
 
